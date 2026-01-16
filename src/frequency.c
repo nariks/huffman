985207486d1da10a0952589a64f103800377769b
Narayan D <narayand@gmail.com>
2026-01-16 15:41:15 +1100

    Phase1:Frequency Analysis inital commit

--- src/frequency.c
+++ src/frequency.c
@@ -1,5 +1,8 @@
 #include "frequency.h"
 #include <stdio.h>
+#include <stdbool.h>
+
+#define BUFFER_SIZE 4096
 
 int calculate_frequencies(const char *filepath, FrequencyMap *map) {
     FILE *file = fopen(filepath, "rb");
@@ -15,7 +18,24 @@ int calculate_frequencies(const char *filepath, FrequencyMap *map) {
 
     // TODO: Implement Buffered I/O here using fread()
     // Loop through the file, update map->counts[byte]
-    
+    uint8_t buffer[BUFFER_SIZE];
+    int readCount;
+    bool emptyFile = true;
+
+    while((readCount = fread(buffer, sizeof(char), BUFFER_SIZE, file)) > 0) {
+        
+        for(int i = 0; i < readCount; i++) {
+            map->counts[buffer[i]]++;
+        }
+
+        if (emptyFile)
+            emptyFile = false;
+    }
+
+    if (emptyFile)
+        printf("File is empty!\n");
+        
     fclose(file);
+
     return 0;
 }
